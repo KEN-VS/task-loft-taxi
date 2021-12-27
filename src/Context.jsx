c77e6c553442d2eedb64310509212b23c1574473
ken-vs <korobitsynae@gmail.com>
2021-12-27 10:29:11 +0300

    1/2 HW 2week (contex upd)

--- src/Context.jsx
+++ src/Context.jsx
@@ -2,14 +2,6 @@ import React from "react";
 
 const Context = React.createContext()
 
-const dataContext = React.createContext({
-  isLoggedIn: false,
-  login() {
-    this.isLoggedIn = true
-  },
-  logout() {
-    this.isLoggedIn = false
-  },
-})
 
-export { Context, dataContext }
+
+export default Context
